d8a357e5960154c09fbbc23492e162b897436c41
Denis Kim <denis.kim666@gmail.com>
2022-06-10 23:41:41 +0600

    Third task added

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня: ");
+
+int numberOfDay = Convert.ToInt32(Console.ReadLine());
+
+if (numberOfDay > 7 || numberOfDay < 1)
+    Console.WriteLine(-1);
+    else if (numberOfDay == 6 || numberOfDay == 7){
+        Console.Write(numberOfDay);
+        Console.WriteLine(" день недели является выходным.");
+    } else{
+        Console.Write(numberOfDay);
+        Console.WriteLine(" день недели является будним днем.");
+    }
+
