77f9fd093a3fd9c514fae9c7ee3df0dd7dcbfd33
Denis Kim <denis.kim666@gmail.com>
2022-06-10 19:46:12 +0600

    First task added

---
+++ FirstTask/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int secondValue = ((number % 100) / 10);
+
+Console.WriteLine(secondValue);
